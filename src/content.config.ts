1823bb45c42d66a06fd7b403cd74714996e5923f
Aaron <aaron@cute.engineer>
2026-09-16 15:40:34 +1000

    Update post page, favicon

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,9 +3,8 @@ import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
 const posts = defineCollection({
-  loader: glob({ base: "./src/pages/posts", pattern: "**/*.{md,mdx}" }),
+  loader: glob({ base: "./src/posts", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
-    id: z.string(),
     title: z.string(),
   }),
 });
